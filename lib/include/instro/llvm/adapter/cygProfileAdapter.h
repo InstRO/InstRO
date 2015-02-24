8585481ad87909e42202ae27483b87451247887a
Jan Lehr <jp.lehr@gmail.com>
2015-02-24 17:15:35 +0100

    I completely formatted the files I am working on with my clang formatter.
    I changed code within the CygProfileAdapter to reflect the InstRO design.
    I added the CashesSelector - which is the basis for the Shadow Stack instrumentation.

--- lib/include/instro/llvm/adapter/cygProfileAdapter.h
+++ lib/include/instro/llvm/adapter/cygProfileAdapter.h
@@ -8,51 +8,41 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include "cashesSelector.h"
-
-
 /*
  * Implements the cyg profile function adapter.
  * This version relies on the cashes selector.
  */
-class CygProfileAdapter : public ::InstRO::LLVM::Pass, public llvm::FunctionPass {
+class CygProfileAdapter : public ::InstRO::LLVM::Pass,
+													public llvm::FunctionPass {
+ public:
+	CygProfileAdapter(::InstRO::LLVM::Pass *inputSel);
 
-	public:
-		CygProfileAdapter(::InstRO::LLVM::Pass *inputSel);
+	const char *getPassName() const { return pn.c_str(); }
 
-  	const char *getPassName()const {
-			return pn.c_str();
-  	}
+	bool doInitialization(llvm::Module &m);
+	bool runOnFunction(llvm::Function &f) override;
 
-  	bool doInitialization(llvm::Module &m);
-  	bool runOnFunction(llvm::Function &f) override;
+	bool doFinalization(llvm::Module &m) {}
 
-  	bool doFinalization(llvm::Module &m) {
-  	}
+	void getAnalysisUsage(llvm::AnalysisUsage &info) const {}
 
-  	void getAnalysisUsage(llvm::AnalysisUsage &info) const {
-			info.addRequired<CashesSelector>();
-  	}
+	static char ID;
 
-		static char ID;
+ private:
+	const std::string pn;
+	const std::string exitName;
+	const std::string entryName;
 
-	private:
-  	const std::string pn;
+	llvm::CallInst *entryFunc;
+	llvm::CallInst *exitFunc;
+	llvm::Module *mod;
+	llvm::CallInst *buildEntryCall(llvm::Function &f);
+	llvm::CallInst *buildExitCall(llvm::Function &f, llvm::ReturnInst *ri);
+	// XXX Why does that need to be a llvm::Twine&& ?
+	llvm::CallInst *buildTCall(llvm::Function &f, llvm::Twine &&name,
+														 llvm::Instruction *insertBefore);
 
-  	llvm::CallInst *entryFunc;
-  	llvm::CallInst *exitFunc;
-  	llvm::Module *mod;
-
-  	const static std::string exitName;
-  	const static std::string entryName;
-
-  	llvm::CallInst *buildEntryCall(llvm::Function &f);
-  	llvm::CallInst *buildExitCall(llvm::Function &f, llvm::ReturnInst *ri);
-  	// XXX Why does that need to be a llvm::Twine&& ?
-  	llvm::CallInst *buildTCall(llvm::Function &f, llvm::Twine &&name, llvm::Instruction *insertBefore);
-
-  	llvm::Function *buildFunction(std::string fName);
+	llvm::Function *buildFunction(std::string fName);
 };
-
 
 #endif
--- lib/src/llvm/adapter/cygProfileAdapter.cpp
+++ lib/src/llvm/adapter/cygProfileAdapter.cpp
@@ -1,89 +1,101 @@
 #include "cygProfileAdapter.h"
 
-
 char CygProfileAdapter::ID = 0;
 
 CygProfileAdapter::CygProfileAdapter(::InstRO::LLVM::Pass *inputSel)
-    : exitName("__cyg_profile_func_exit"),
-      entryName("__cyg_profile_func_enter") {}
+		: pn("CygProfileAdapter"),
+			exitName("__cyg_profile_func_exit"),
+			entryName("__cyg_profile_func_enter") {}
 
 bool CygProfileAdapter::doInitialization(llvm::Module &m) {
-  mod = &m;
-  llvm::Function *func = buildFunction(exitName);
-  mod->getOrInsertFunction(exitName, func->getFunctionType());
-  func = buildFunction(entryName);
-  mod->getOrInsertFunction(entryName, func->getFunctionType());
+	mod = &m;
+	llvm::Function *func = buildFunction(exitName);
+	mod->getOrInsertFunction(exitName, func->getFunctionType());
+	func = buildFunction(entryName);
+	mod->getOrInsertFunction(entryName, func->getFunctionType());
 }
 
 bool CygProfileAdapter::runOnFunction(llvm::Function &f) {
-  CashesSelector &defSel = getAnalysis<CashesSelector>();
-  auto res = std::find(std::begin(defSel.selectionSet), std::end(defSel.selectionSet), &f);
-  if(res != std::end(defSel.selectionSet)) {
-    entryFunc = buildEntryCall(f);
-    for(auto &bb : f.getBasicBlockList()) {
-      llvm::BasicBlock::InstListType &instructions = bb.getInstList();
-      for(auto &i : instructions) {
-        if(llvm::isa<llvm::ReturnInst>(i)) {
-          llvm::ReturnInst *ti = llvm::dyn_cast<llvm::ReturnInst>(&i);
-          exitFunc = buildExitCall(f, ti);
-        }
-      }
-    }
-    // we insert the call to the entry function
-    llvm::BasicBlock &bb = f.getEntryBlock();
-    bb.getInstList().insert(bb.getFirstInsertionPt(), entryFunc);
-    return true;
-  } else {
-    return false;
-  }
+	// This method is implemented in InstRO::LLVM::Pass to be able
+	// to abstract away a map, which is used by the pass manager to maybe
+	// fool the pass requesting the construct set.
+	auto &cs = getInput(inputSelector);
+	auto constructs = cs.get();
+	// If the function was marked for adaption
+	auto res = std::find(std::begin(constructs), std::end(constructs), &f);
+	// we build the instrumentation
+	if (res != std::end(constructs)) {
+		entryFunc = buildEntryCall(f);
+		for (auto &bb : f.getBasicBlockList()) {
+			llvm::BasicBlock::InstListType &instructions = bb.getInstList();
+			for (auto &i : instructions) {
+				if (llvm::isa<llvm::ReturnInst>(i)) {
+					llvm::ReturnInst *ti = llvm::dyn_cast<llvm::ReturnInst>(&i);
+					exitFunc = buildExitCall(f, ti);
+				}
+			}
+		}
+		// we insert the call to the entry function
+		llvm::BasicBlock &bb = f.getEntryBlock();
+		bb.getInstList().insert(bb.getFirstInsertionPt(), entryFunc);
+		return true;
+	} else {
+		return false;
+	}
 }
 
-llvm::CallInst *CygProfileAdapter::buildExitCall(llvm::Function &f, llvm::ReturnInst *ri) {
-  return buildTCall(f, llvm::Twine(exitName), ri);
+llvm::CallInst *CygProfileAdapter::buildExitCall(llvm::Function &f,
+																								 llvm::ReturnInst *ri) {
+	return buildTCall(f, llvm::Twine(exitName), ri);
 }
 
 llvm::CallInst *CygProfileAdapter::buildEntryCall(llvm::Function &f) {
-  return buildTCall(f, llvm::Twine(entryName), nullptr);
+	return buildTCall(f, llvm::Twine(entryName), nullptr);
 }
 
 llvm::Function *CygProfileAdapter::buildFunction(std::string name) {
-  llvm::PointerType *voidPtr = llvm::PointerType::getUnqual(llvm::Type::getInt8Ty(mod->getContext()));
-  std::vector<llvm::Type *> argTypes(2, voidPtr);
-  llvm::ArrayRef<llvm::Type *> argsT(argTypes);
+	llvm::PointerType *voidPtr =
+			llvm::PointerType::getUnqual(llvm::Type::getInt8Ty(mod->getContext()));
+	std::vector<llvm::Type *> argTypes(2, voidPtr);
+	llvm::ArrayRef<llvm::Type *> argsT(argTypes);
 
-  // We get ourselves the pointer to the function type
-  llvm::FunctionType *fType = llvm::FunctionType::get(llvm::Type::getVoidTy(mod->getContext()), argsT,
-                              false);
+	// We get ourselves the pointer to the function type
+	llvm::FunctionType *fType = llvm::FunctionType::get(
+			llvm::Type::getVoidTy(mod->getContext()), argsT, false);
 
-  // Creates a new llvm Function object, that can be used inside a call instruction
-  llvm::Function *func = llvm::Function::Create(fType, llvm::GlobalValue::ExternalLinkage,
-                         llvm::Twine(name));
-  return func;
+	// Creates a new llvm Function object, that can be used inside a call
+	// instruction
+	llvm::Function *func = llvm::Function::Create(
+			fType, llvm::GlobalValue::ExternalLinkage, llvm::Twine(name));
+	return func;
 }
 
-llvm::CallInst *CygProfileAdapter::buildTCall(llvm::Function &f, llvm::Twine &&name,
-    llvm::Instruction *insertBefore) {
-  // constructs a ptr to void type in the general address space
-  llvm::PointerType *voidPtr = llvm::PointerType::getUnqual(llvm::Type::getInt8Ty(mod->getContext()));
-  std::vector<llvm::Type *> argTypes(2, voidPtr);
-  llvm::ArrayRef<llvm::Type *> argsT(argTypes);
+llvm::CallInst *CygProfileAdapter::buildTCall(llvm::Function &f,
+																							llvm::Twine &&name,
+																							llvm::Instruction *insertBefore) {
+	// constructs a ptr to void type in the general address space
+	llvm::PointerType *voidPtr =
+			llvm::PointerType::getUnqual(llvm::Type::getInt8Ty(mod->getContext()));
+	std::vector<llvm::Type *> argTypes(2, voidPtr);
+	llvm::ArrayRef<llvm::Type *> argsT(argTypes);
 
-  llvm::FunctionType *fType = llvm::FunctionType::get(llvm::Type::getVoidTy(mod->getContext()), argsT,
-                              false);
-  // Get a pointer to the function
-  llvm::Constant *castExpr = llvm::ConstantExpr::getPointerCast(&f, voidPtr);
-  llvm::Constant *callsite = llvm::ConstantPointerNull::get(voidPtr);
+	llvm::FunctionType *fType = llvm::FunctionType::get(
+			llvm::Type::getVoidTy(mod->getContext()), argsT, false);
+	// Get a pointer to the function
+	llvm::Constant *castExpr = llvm::ConstantExpr::getPointerCast(&f, voidPtr);
+	llvm::Constant *callsite = llvm::ConstantPointerNull::get(voidPtr);
 
-  // build a vector holding the call argument list
-  std::vector<llvm::Value *> a;//(2, castExpr);
-  a.push_back(castExpr);
-  a.push_back(callsite);
-  llvm::ArrayRef< llvm::Value *> args(a);
-  llvm::Twine n("");
-  llvm::Constant *func = mod->getOrInsertFunction(name.getSingleStringRef(), fType);
+	// build a vector holding the call argument list
+	std::vector<llvm::Value *> a;	//(2, castExpr);
+	a.push_back(castExpr);
+	a.push_back(callsite);
+	llvm::ArrayRef<llvm::Value *> args(a);
+	llvm::Twine n("");
+	llvm::Constant *func =
+			mod->getOrInsertFunction(name.getSingleStringRef(), fType);
 
-  llvm::CallInst *callInst = llvm::CallInst::Create(func, args, n, insertBefore);
-  return callInst;
+	llvm::CallInst *callInst =
+			llvm::CallInst::Create(func, args, n, insertBefore);
+	return callInst;
 }
-
 
