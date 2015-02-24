266cdc8fb454cdb0398850a5b4fa0d9cf7734c88
Jan Lehr <jp.lehr@gmail.com>
2015-02-24 17:26:20 +0100

    I added missing namespace delcarations and the implementation of the cashes selector was moved into this repository

--- lib/include/instro/llvm/adapter/cygProfileAdapter.h
+++ lib/include/instro/llvm/adapter/cygProfileAdapter.h
@@ -8,41 +8,45 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/Support/raw_ostream.h"
 
-/*
- * Implements the cyg profile function adapter.
- * This version relies on the cashes selector.
- */
-class CygProfileAdapter : public ::InstRO::LLVM::Pass,
-													public llvm::FunctionPass {
- public:
-	CygProfileAdapter(::InstRO::LLVM::Pass *inputSel);
+namespace InstRO {
+	namespace LLVM {
+		/*
+		 * Implements the cyg profile function adapter.
+		 * This version relies on the cashes selector.
+		 */
+		class CygProfileAdapter : public ::InstRO::LLVM::Pass,
+															public llvm::FunctionPass {
+		 public:
+			CygProfileAdapter(::InstRO::LLVM::Pass *inputSel);
 
-	const char *getPassName() const { return pn.c_str(); }
+			const char *getPassName() const { return pn.c_str(); }
 
-	bool doInitialization(llvm::Module &m);
-	bool runOnFunction(llvm::Function &f) override;
+			bool doInitialization(llvm::Module &m);
+			bool runOnFunction(llvm::Function &f) override;
 
-	bool doFinalization(llvm::Module &m) {}
+			bool doFinalization(llvm::Module &m) {}
 
-	void getAnalysisUsage(llvm::AnalysisUsage &info) const {}
+			void getAnalysisUsage(llvm::AnalysisUsage &info) const {}
 
-	static char ID;
+			static char ID;
 
- private:
-	const std::string pn;
-	const std::string exitName;
-	const std::string entryName;
+		 private:
+			const std::string pn;
+			const std::string exitName;
+			const std::string entryName;
 
-	llvm::CallInst *entryFunc;
-	llvm::CallInst *exitFunc;
-	llvm::Module *mod;
-	llvm::CallInst *buildEntryCall(llvm::Function &f);
-	llvm::CallInst *buildExitCall(llvm::Function &f, llvm::ReturnInst *ri);
-	// XXX Why does that need to be a llvm::Twine&& ?
-	llvm::CallInst *buildTCall(llvm::Function &f, llvm::Twine &&name,
-														 llvm::Instruction *insertBefore);
+			llvm::CallInst *entryFunc;
+			llvm::CallInst *exitFunc;
+			llvm::Module *mod;
+			llvm::CallInst *buildEntryCall(llvm::Function &f);
+			llvm::CallInst *buildExitCall(llvm::Function &f, llvm::ReturnInst *ri);
+			// XXX Why does that need to be a llvm::Twine&& ?
+			llvm::CallInst *buildTCall(llvm::Function &f, llvm::Twine &&name,
+																 llvm::Instruction *insertBefore);
 
-	llvm::Function *buildFunction(std::string fName);
-};
+			llvm::Function *buildFunction(std::string fName);
+		};
 
+	}	// LLVM
+}	// InstRO
 #endif
