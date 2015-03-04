c13e885da0b543182147c3f567064944043c59cc
Jan Lehr <jp.lehr@gmail.com>
2015-03-04 15:42:00 +0100

    I made it compile again. In theory, this would eben create an instance of a clang based InstRO. However, a lot of methods are not implemented yet.

--- lib/include/instro/clang/core/PassFactory.h
+++ lib/include/instro/clang/core/PassFactory.h
@@ -3,9 +3,19 @@
 
 #include "instro/core/PassFactory.h"
 
+#include "instro/clang/core/Pass.h"
+
 namespace InstRO {
 namespace Clang {
-class PassFactory : public ::InstRO::Core::PassFactory {};
+class PassFactory : public ::InstRO::Core::PassFactory {
+ public:
+ 	PassFactory(::InstRO::Core::PassManagement::PassManager *manager) : ::InstRO::Core::PassFactory(manager){};
+	Pass* createBlackAndWhiteListSelector(
+			std::vector<std::string> rules) {};
+	Pass* createBooleanOrSelector(::InstRO::Pass* inputA, ::InstRO::Pass* inputB) {};
+	Pass* createProgramEntrySelector() {};
+	Pass* createCygProfileAdapter(::InstRO::Pass* input) {};
+};
 }	// Clang
 }	// INstRO
 #endif
