37c81c5f14cf76d6fab1388726fcd59b2a070268
Jan Lehr <jp.lehr@gmail.com>
2015-03-05 22:44:30 +0100

    Added a util class which can be used by the BlackWhitelist Selectors to parse the configuration file. This is compiler agnostic and can be used with every BWL implementation.

---
+++ lib/src/support/BWLFileReader.cpp
@@ -0,0 +1,50 @@
+
+#include "BWLFileReader.h"
+
+using namespace InstRO;
+
+util::BWLFileReader::BWLFileReader(std::string filename) : wSpecifier("+"), bSpecifier("-"), filename(filename){
+}
+
+std::pair<std::vector<std::string>, std::vector<std::string> > 
+util::BWLFileReader::getBWList(){
+	if(! hasBeenRead){
+		readFile();
+	}
+
+	return std::make_pair(blacklist, whitelist);
+}
+
+void util::BWLFileReader::readFile(){
+	if(filename.empty()){
+		std::cerr << "InstRO Error: The filename was empty in BWLFileReader\n" << std::endl;
+		return;
+	}
+	std::ifstream inFile;
+	inFile.open(filename.c_str());
+
+	std::string tStr;
+	while(inFile.good()) {
+		
+		std::getline(inFile, tStr);
+
+		if(boost::starts_with(tStr, wSpecifier)) {
+			whitelist.push_back(prepareName(tStr, wSpecifier));
+		} else if(boost::starts_with(tStr, bSpecifier)) {
+			blacklist.push_back(prepareName(tStr, bSpecifier));
+		}
+	}
+
+	inFile.close();
+}
+
+std::string util::BWLFileReader::prepareName(std::string name, std::string specifier){
+	name.erase(0, specifier.length());
+	boost::trim(name);
+
+	if(!boost::starts_with("::", name)){
+		return "::" + name;
+	}
+
+	return name;
+}
