c0a9dbe2312991db41fadf911149004fd37499f6
Jan Lehr <jp.lehr@gmail.com>
2015-03-24 15:48:28 +0100

    I introduced a templatized registration thing, how we could register templatized Passes in LLVM. This is, as well, part of the prototypic implementation of the LLVM world InstRO.

--- lib/src/llvm/Register.cpp
+++ lib/src/llvm/Register.cpp
@@ -4,39 +4,54 @@
 #include "instro/llvm/selector/CashesSelector.h"
 #include "instro/llvm/adapter/CygProfileAdapter.h"
 #include "instro/llvm/adapter/CygProfileAdapterT.h"
+#include "instro/llvm/selector/ClangFrontendSelector.h"
 
 #include "llvm/PassInfo.h"
-
 
 template <typename pass, typename... Args>
 llvm::Pass *create(Args... ctorArgs) {
 	return new pass(ctorArgs...);
 }
 
-llvm::Pass *createCygProfAndCashesSel(){
-return new InstRO::LLVM::CygProfileAdapterT<InstRO::LLVM::CashesSelector>();
-//return create<InstRO::LLVM::CygProfileAdapter>(new InstRO::LLVM::CashesSelector());
+template <typename P, template <typename> class D>
+llvm::Pass *create() {
+	return new D<P>();
 }
 
+/*
+ * Instantiation helper used to register passes with dependencies in clang
+ *
+ * TODO have the Dependence register itself
+ */
+template <template <typename> class Pass, typename Dependence>
+struct InstantiationHelper {
+	typedef Pass<Dependence> InstanceT;
+	static llvm::Pass *create() { return new Pass<Dependence>(); }
+};
+
+llvm::Pass *createCygProfAndCashesSel() {
+	return create<InstRO::LLVM::ClangFrontendSelector, InstRO::LLVM::CygProfileAdapterT>();
+}
 
 template <typename T>
 struct InstRORegisterPass : public llvm::PassInfo {
 	InstRORegisterPass(const char *passArg, const char *name, bool CFGOnly = false, bool isAnalysis = false)
-			: llvm::PassInfo(
-						name, passArg, &T::ID,
-						llvm::PassInfo::NormalCtor_t(createCygProfAndCashesSel),
-						CFGOnly, isAnalysis) {
+			: llvm::PassInfo(name, passArg, &T::InstanceT::ID, llvm::PassInfo::NormalCtor_t(T::create), CFGOnly, isAnalysis) {
 		llvm::PassRegistry::getPassRegistry()->registerPass(*this);
-	std::cout << "In InstRORegisterPass constructor" << std::endl;
+		std::cout << "In InstRORegisterPass constructor" << std::endl;
 	}
 };
-
-
 
 /*
  * This is to register the passes inside the modular optimizer
  */
-InstRORegisterPass<InstRO::LLVM::CygProfileAdapterT<InstRO::LLVM::CashesSelector> > X("foobar", "CygProfileAdapter World Pass");
+// InstRORegisterPass<InstRO::LLVM::CygProfileAdapterT<InstRO::LLVM::CashesSelector> > X("foobar",	"CygProfileAdapter
+// World Pass");
 
 llvm::RegisterPass<InstRO::LLVM::CashesSelector> Y("cash", "instro cash");
+
+llvm::RegisterPass<InstRO::LLVM::ClangFrontendSelector> Z("asd", "sssks");
+
+InstRORegisterPass<InstantiationHelper<InstRO::LLVM::CygProfileAdapterT, InstRO::LLVM::ClangFrontendSelector> > Test(
+		"asdf", "asdasfwe");
 
