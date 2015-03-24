a581247741af1f3f246d434fa54d0705958e6098
Jan Lehr <jp.lehr@gmail.com>
2015-03-24 09:53:20 +0100

    I introduced the second, non-visiting executer, into the clang hierarchy. No we can have PassImplementations be run without traversing the AST. In Clang this can actually make sense, since we do not necessarily store information attached to the AST.

--- lib/src/clang/support/ClangConsumerFactory.cpp
+++ lib/src/clang/support/ClangConsumerFactory.cpp
@@ -1,22 +1,20 @@
 #include "instro/clang/core/ClangConsumerFactory.h"
 
 InstRO::Clang::Support::ClangConsumerFactory::ClangConsumerFactory(
-		InstRO::PassManagement::PassManager *manager,
-		clang::tooling::Replacements &replacements, InstRO::Clang::PassManagement::ClangPassExecuter *executer)
-		: manager(manager), executer(executer), replacements(replacements) {}
+		InstRO::PassManagement::PassManager *manager, clang::tooling::Replacements &replacements,
+		InstRO::Clang::PassManagement::ClangPassExecuter *vExecuter, InstRO::Clang::PassManagement::ClangPassExecuter *nvExecuter)
+		: manager(manager), vExecuter(vExecuter), nvExecuter(nvExecuter), replacements(replacements) {}
 
 InstRO::Clang::Support::ClangConsumerFactory::~ClangConsumerFactory() {}
 
-std::unique_ptr<clang::ASTConsumer>
-InstRO::Clang::Support::ClangConsumerFactory::newASTConsumer() {
+std::unique_ptr<clang::ASTConsumer> InstRO::Clang::Support::ClangConsumerFactory::newASTConsumer() {
 	std::cout << "Creating new ASTConsumer" << std::endl;
 	return std::unique_ptr<InstRO::Clang::Support::InstROASTConsumer>(
-			new InstRO::Clang::Support::InstROASTConsumer(manager, executer));
+			new InstRO::Clang::Support::InstROASTConsumer(manager, vExecuter, nvExecuter));
 }
 
 std::unique_ptr<clang::ASTConsumer> InstRO::Clang::Support::ClangConsumerFactory::CreateASTConsumer(
-			clang::CompilerInstance &ci, llvm::StringRef strRef){
+		clang::CompilerInstance &ci, llvm::StringRef strRef) {
 	return std::unique_ptr<clang::ASTConsumer>(nullptr);
 }
-
 
