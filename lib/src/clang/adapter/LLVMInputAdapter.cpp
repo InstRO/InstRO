c8e97eaf2626ddcb4b95d12a0801aaea5b5da7ee
Jan Lehr <jp.lehr@gmail.com>
2015-03-24 10:01:02 +0100

    I introduced a new Adpater - LLVMInputAdapter. The adapter outputs a file with the selected node with their "normal" names as well as their mangled names. This can be used to use the selection information at the LLVM function level.

---
+++ lib/src/clang/adapter/LLVMInputAdapter.cpp
@@ -0,0 +1,41 @@
+#include "instro/clang/adapter/LLVMInputAdapter.h"
+
+InstRO::Clang::LLVMInputAdapter::LLVMInputAdapter(InstRO::Pass *inputPass)
+		: decidingSelector(inputPass), outfileName("instro-temp-file") {}
+
+
+bool InstRO::Clang::LLVMInputAdapter::VisitFunctionDecl(clang::FunctionDecl *fDecl){}
+
+void InstRO::Clang::LLVMInputAdapter::init() {}
+
+void InstRO::Clang::LLVMInputAdapter::finalize() {}
+
+void InstRO::Clang::LLVMInputAdapter::releaseOutput() {}
+
+void InstRO::Clang::LLVMInputAdapter::execute(){
+	executer->execute(this);
+}
+
+void InstRO::Clang::LLVMInputAdapter::exec() {
+	std::cout << "Running exec" << std::endl;
+	if(context == nullptr){
+		std::cerr << "Context was nullptr" << std::endl;
+		exit(-1);
+	}
+	std::ofstream outStream;
+	outStream.open(outfileName.c_str());
+
+	if (outStream) {
+		auto inputSet = getInput(decidingSelector);
+		if(inputSet == nullptr){
+			std::cerr << "inputSet was nullptr" << std::endl;
+			exit(-1);
+		}
+		auto cs = reinterpret_cast<InstRO::Clang::ClangConstructSet *>(inputSet);
+		InstRO::Clang::print(outStream, cs, context);
+	}
+
+	outStream.close();
+}
+
+InstRO::Clang::ClangConstructSet *InstRO::Clang::LLVMInputAdapter::getOutput() { return nullptr; }
