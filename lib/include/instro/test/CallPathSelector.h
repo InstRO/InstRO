3a45a2618c0fe8cc3bae18095840e556d92a038f
ciwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-06-20 20:10:28 +0200

    Continue to Work on the PassImplementation. Checkin to move to other machine...

--- lib/include/instro/test/CallPathSelector.h
+++ lib/include/instro/test/CallPathSelector.h
@@ -1,18 +1,21 @@
+#include "instro/core/PassImplementation.h"
 #include "instro/test/TestPass.h"
 
 
 namespace InstRO
 {
+
 	namespace Test
 	{
 		namespace Selectors
 		{
 			class CallPathSelector :public TestPass{
+				void foor(){}
 			protected:
 				Pass * fromPass, *toPass;
 				CallPathSelector() = delete;
 			public:
-				CallPathSelector(Pass * from, Pass * to):fromPass(from),toPass(to){};
+				CallPathSelector(Pass * from, Pass * to):InstRO::Core::PassImplementation(ChannelConfiguration()), fromPass(from), toPass(to){};
 				virtual void init() override;
 				virtual void execute() override;
 				virtual void finalize() override ;
