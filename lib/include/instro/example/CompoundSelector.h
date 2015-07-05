1a9577ca198e83584b04a2376793e1733d50c754
Christian Iwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-07-05 16:43:39 +0200

    Added a dummy-compound-selector to the example project. Will use it to specify behavior.

---
+++ lib/include/instro/example/CompoundSelector.h
@@ -0,0 +1,36 @@
+#ifndef ROSE_COMPOUNDSELECTORS_H
+#define ROSE_COMPOUNDSELECTORS_H
+#include <vector>
+#include <string>
+#include <memory>
+#include "instro/example/ExamplePass.h"
+#include "instro/core/Pass.h"								 // Pass
+#include "instro/core/PassImplementation.h"	// ChannelConfiguration
+
+namespace InstRO {
+	namespace Example {
+		namespace Selectors {
+
+			class CompoundSelector : public InstRO::Example::ExamplePass {
+			protected:
+				std::unique_ptr<InstRO::Core::ConstructSet> passOutput;
+			public:
+				typedef enum {
+					CO_Or,
+					CO_And,
+					CO_XOr
+				} CompoundOperationType;
+				CompoundSelector(Pass* inputA, Pass* inputB, CompoundOperationType Operation)
+					: ExamplePass(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
+				CompoundSelector(Pass* inputA, bool invertPassA, Pass* inputB, bool invertPassB, CompoundOperationType Operation)
+					: ExamplePass(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
+				void init() override {};
+				void execute() override;
+				void finalize() override{};
+				void releaseOutput(){ passOutput->clear(); };
+				InstRO::Core::ConstructSet* getOutput() override { return passOutput.get(); }
+			};
+		}	// End namespace Selectors
+	}	// End namespace Rose
+}	// End namepsace InstRO
+#endif
---
+++ lib/src/example/CompoundSelector.cpp
@@ -0,0 +1,14 @@
+#include "instro/example/CompoundSelector.h"
+
+namespace InstRO {
+	namespace Example {
+		namespace Selectors {
+
+			void CompoundSelector::execute()
+			{
+
+			}
+		}
+
+	}
+}
