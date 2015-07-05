52fe4d051e29664c41d3a02286b68ec101ca1414
Christian Iwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-07-05 15:04:46 +0200

    Updates. Need to rename to CompoundSelector.h

---
+++ lib/src/rose/selectors/CompoundSelector.cpp
@@ -0,0 +1,14 @@
+#include "instro/rose/selectors/CompoundSelector.h"
+
+namespace InstRO {
+	namespace Rose {
+		namespace Selectors {
+
+			void CompoundSelector::execute()
+			{
+
+			}
+		}
+
+	}
+}
