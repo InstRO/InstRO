90ba8393c91065359a0af94d65c5779c899ba443
ciwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-07-06 14:33:48 +0200

    Prototyped the ConstructElevator for ROSE

---
+++ lib/include/instro/rose/tooling/RoseConstructElevator.h
@@ -0,0 +1,28 @@
+#ifndef INSTRO_ROSE_CONSTRUCT_ELEVATOR
+#define INSTRO_ROSE_CONSTRUCT_ELEVATOR
+
+#include <memory>	// We need shared pointers
+#include <list>		 // We use List in the GrammarInterface
+#include "instro/core/ConstructSet.h"
+#include "instro/tooling/ConstructElevator.h"
+
+
+namespace InstRO {
+namespace Rose{
+namespace Tooling {
+namespace ConstructElevator {
+/* CI: The ConstructSet class is intended to be specialized for each compiler interface. It provides the basic
+* mechanisms to specify what construct level are contained. */
+class ConstructElevator: public InstRO::Tooling::ConstructElevator::ConstructElevator {
+protected:
+public:
+	// This is the implicit way that the PassManager will allways apply
+	virtual std::unique_ptr<InstRO::Core::ConstructSet> raise(InstRO::Core::ConstructSet *input, InstRO::Core::ConstructLevelType cl) override;
+	// This is an explicit function used in very rare circumstances by e.g. a specialized selection pass (if at all)
+	virtual std::unique_ptr<InstRO::Core::ConstructSet> lower(InstRO::Core::ConstructSet *input, InstRO::Core::ConstructLevelType cl) override;
+};
+}
+}
+}
+}
+#endif
---
+++ lib/include/instro/tooling/ControlFlowGraph.h
@@ -0,0 +1,16 @@
+#include <memory>	// We need shared pointers
+#include <list>		 // We use List in the GrammarInterface
+#include "instro/core/ConstructSet.h"
+
+namespace InstRO {
+	namespace Tooling {
+		namespace ControlFlowGraph {
+			class ControlFlowGraphNode {
+			public:
+				// CI: Explicit RAW Pointer. We do not release control of this CS
+				virtual Core::ConstructSet *getAssociatedConstructSet() = 0;
+			};
+			class ControlFlowGraph {};
+		}
+	}
+}
