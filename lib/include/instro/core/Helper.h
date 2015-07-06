a12d45ce3e6fa3c95a9503cb572d1c2163935198
C. Iwainsky <iwainsky@sc.tu-darmstadt.de>
2015-07-06 14:45:24 +0200

    CI: added linux changes

--- lib/include/instro/core/Helper.h
+++ lib/include/instro/core/Helper.h
@@ -3,8 +3,8 @@
 namespace std {
 
 #ifdef PROVIDE_MAKE_UNIQUE
-
-template<typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args);
+// template<typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args);
+template<typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args) {return std::unique_ptr<T>(new T(std::forward<Args>(args)...));}
 
 #endif
 }
