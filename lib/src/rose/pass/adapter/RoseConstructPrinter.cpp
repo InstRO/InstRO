e140393e39080c0970b59bb75141360aa25ce3f6
ciwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-07-06 17:43:39 +0200

    Starting a big renaming and folder restructuring session

---
+++ lib/src/rose/pass/adapter/RoseConstructPrinter.cpp
@@ -0,0 +1,27 @@
+#include "instro/core/Pass.h"
+#include "instro/rose/core/RoseConstructSet.h"
+#include "instro/rose/pass/adapter/RoseConstructPrinter.h"
+
+namespace InstRO {
+namespace Rose {
+namespace Adapter {
+
+void ExampleConstructPrinter::execute(){
+	InstRO::InfracstructureInterface::ConstructSetCompilerInterface cs(inputPass->getOutput());
+	/*InstRO::Example::Core::InfracstructureInterface::ExampleConstructSet * pecs =
+	dynamic_cast<InstRO::Example::Core::InfracstructureInterface::ExampleConstructSet *>(ocs);
+	InstRO::Example::Core::InfracstructureInterface::ExampleConstructSet cs = *(pecs);*/
+	std::cout << "ConstructSet contains " << cs.size() << " constructs:" << std::endl;
+	unsigned long count = 0;
+	for (auto construct : cs) {
+		InstRO::Core::Construct* pc = construct.get();
+		InstRO::Rose::Core::RoseConstruct* ec =
+			dynamic_cast<InstRO::Rose::Core::RoseConstruct *>(construct.get());
+		std::cout << count << ":\t Level " << ec->getLevel() << "\t:" << ec->getNode()->unparseToString() << std::endl;
+		count++;
+	}
+}
+
+}
+}
+}
