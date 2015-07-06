40e1a70df8cd457baa3f140707528d236961c3bf
ciwainsky <christian.iwainsky@sc.tu-darmstadt.de>
2015-07-06 09:35:05 +0200

    My ReImplementation of make_unique caused some problems... I guarded it with an ifdef, that we can set in Linux for G++. Maybe, we should increase our requirements to C++14?

--- lib/include/instro/core/Helper.h
+++ lib/include/instro/core/Helper.h
@@ -1,7 +1,11 @@
 #ifndef INSTRO_STD_HELPER
 #define INSTRO_STD_HELPER
 namespace std {
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args);
+
+#ifdef PROVIDE_MAKE_UNIQUE
+
+template<typename T, typename... Args> std::unique_ptr<T> make_unique(Args&&... args);
+
+#endif
 }
 #endif
