ffd96581464e857b1fc256e98b06a7a32bcbb7de
ciwainsky <ciwainsky@github.com>
2015-07-21 14:58:11 +0200

    Updated ConstructElevator to return copies of ConstructSets instread of pointers and to accept const references Added ReadOnlyConstructSetCompilerInterface for use with const ConstructSets

--- lib/include/instro/tooling/ConstructElevator.h
+++ lib/include/instro/tooling/ConstructElevator.h
@@ -15,14 +15,18 @@ class ConstructElevator {
 	virtual ~ConstructElevator() {}
 
 	// This is the implicit way that the PassManager will allways apply
-	virtual std::unique_ptr<Core::ConstructSet> raise(Core::ConstructSet *input, Core::ConstructTraitType cl) = 0;
+	virtual Core::ConstructSet raise(const Core::ConstructSet *input, Core::ConstructTraitType cl) = 0;
+	virtual Core::ConstructSet raise(const Core::ConstructSet & input, Core::ConstructTraitType cl) = 0;
 	// This is an explicit function used in very rare circumstances by e.g. a specialized selection pass (if at all)
-	virtual std::unique_ptr<Core::ConstructSet> lower(Core::ConstructSet *input, Core::ConstructTraitType cl) = 0;
+	virtual Core::ConstructSet lower(const Core::ConstructSet *input, Core::ConstructTraitType cl) = 0;
+	virtual Core::ConstructSet lower(const Core::ConstructSet &input, Core::ConstructTraitType cl) = 0;
 
 	// Crop Construct up to, or down to a level. This can be implemented compiler independend, as it does not require AST
 	// access
-	virtual std::unique_ptr<Core::ConstructSet> crop(Core::ConstructSet *input, Core::ConstructTraitType min,
+	virtual Core::ConstructSet crop(Core::ConstructSet *input, Core::ConstructTraitType min,
 																									 Core::ConstructTraitType max);
+	virtual Core::ConstructSet crop(const Core::ConstructSet & input, Core::ConstructTraitType min,
+		Core::ConstructTraitType max);
 };
 }
 }
