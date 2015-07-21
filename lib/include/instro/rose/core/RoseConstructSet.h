628ec366f7aee8be3d8c4f5dc14d60e93e6c6c61
ciwainsky <ciwainsky@github.com>
2015-07-21 14:27:04 +0200

    Rose Fragment now inherits from RoseConstruct

--- lib/include/instro/rose/core/RoseConstructSet.h
+++ lib/include/instro/rose/core/RoseConstructSet.h
@@ -235,7 +235,7 @@ class RoseConstruct : public InstRO::Core::Construct {
 
 	SgNode* getNode() const { return node; }
 
-	std::string toString() override {
+	virtual std::string toString() override {
 		return "RoseConstruct: " + node->class_name() + ": " + node->unparseToString();
 	}
 
@@ -243,10 +243,10 @@ class RoseConstruct : public InstRO::Core::Construct {
 	SgNode* node;
 };
 
-class RoseFragment : public InstRO::Core::Construct {
+class RoseFragment : public RoseConstruct {
 public:
-	RoseFragment(Sg_File_Info* info) :
-			InstRO::Core::Construct(InstRO::Core::ConstructTrait(InstRO::Core::ContstructTraitEnum::CTFragment)), info(info) {
+	RoseFragment(SgNode * associatedNode, Sg_File_Info* info) :
+		RoseConstruct(associatedNode,InstRO::Core::ConstructTrait(InstRO::Core::ConstructTraitType::CTFragment)), info(info) {
 	}
 	~RoseFragment() {}
 
