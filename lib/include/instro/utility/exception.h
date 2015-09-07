27d8207703c1d7dca2f054e5c041ebd720cbe574
Simon <simon.reuss90@gmx.de>
2015-09-07 16:57:31 +0200

    Autotools update
    Small cleanup of configure.ac & lib/Makefile.am. Fixed the handling of different flags according to conventions (e.g. include dirs to CPPFLAGS, --std=c++11 to CXXFLAGS).
    Added check for rapidjson (--with-rapidjson=...) to the configure script and modified the Makefile accordingly by adding the include directory and the ConfigInstrumentor target.

--- lib/include/instro/utility/exception.h
+++ lib/include/instro/utility/exception.h
@@ -1,21 +1,25 @@
 #ifndef INSTRO_UTILITY_EXCEPTION
 #define INSTRO_UTILITY_EXCEPTION
 
+#include "instro/config.h"
+
 #include <string>
 #include <iostream>
 #include <cstdlib>
 
 namespace InstRO {
-	
+
+	/// If exceptions are enabled, throws a std::string with the specified message.
+	/// Otherwise the message is fed to standard error and the program aborted.
 	inline void raise_exception(const char* msg) {
-		#if 1
-			throw std::string(msg);
-		#else
+		#if INSTRO_DISABLE_EXCEPTIONS
 			std::cerr << msg << std::endl;
 			std::abort();
+		#else
+			throw std::string(msg);
 		#endif
 	}
-	
+
 	inline void raise_exception(const std::string &msg) {
 		raise_exception(msg.c_str());
 	}
