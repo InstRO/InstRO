17318c3964c77619ab990a0fe9b166065b18cc51
roman <kaossilat0r.rn@gmail.com>
2015-10-02 18:26:23 +0200

    removed obsolete methods from GrammarInterface

--- lib/include/instro/rose/tooling/RoseGrammarInterface.h
+++ lib/include/instro/rose/tooling/RoseGrammarInterface.h
@@ -1,5 +1,3 @@
-#include <memory>	// We need shared pointers
-#include <list>		 // We use List in the GrammarInterface
 #include "instro/core/ConstructSet.h"
 #include "instro/tooling/GrammarInterface.h"
 
@@ -18,12 +16,6 @@ class RoseGrammarInterface : public InstRO::Tooling::GrammarInterface::GrammarIn
  public:
 	RoseGrammarInterface() = delete;
 	RoseGrammarInterface(SgProject *projArg) : proj(projArg) {}
-
-	std::unique_ptr<InstRO::Core::ConstructSet> getConstructsByType(
-			const InstRO::Tooling::GrammarInterface::GrammarTypesType &types) override;
-
-	std::list<InstRO::Tooling::GrammarInterface::GrammarTypesType> getGrammerTypes(
-			const InstRO::Core::ConstructSet &cs) override;
 
 	std::unique_ptr<InstRO::Core::ConstructSet> getConstructsByClass(
 			const InstRO::Core::ConstructTraitType constructClass) override;
