8b83423d847fde08fb29e771d8497c96455ef43e
roman <kaossilat0r.rn@gmail.com>
2015-10-02 18:03:42 +0200

    removed obsolete RoseProgramentrySelector

--- lib/src/pass/selector/ProgramEntrySelector.cpp
+++ lib/src/pass/selector/ProgramEntrySelector.cpp
@@ -4,36 +4,39 @@
 #include "instro/tooling/NamedConstructAccess.h"
 #include "instro/pass/selector/ProgramEntrySelector.h"
 
-#include <iostream>
 #include <string>
 #include <list>
 
 namespace InstRO {
 namespace Selector {
-void ProgramEntrySelector::init() {}
-void ProgramEntrySelector::finalize() {}
-void ProgramEntrySelector::execute() {
-	std::list<std::string> mainRegEx;
-	mainRegEx.push_back(std::string("main"));
-	auto mainMatcher = InstRO::Tooling::NamedConstructAccess::WildcardedStringMatcher(mainRegEx);
+void ProgramEntrySelector::init() {
+}
 
-	std::unique_ptr<InstRO::Core::ConstructSet> csByNameMain = InstRO::getInstrumentorInstance()
-																																 ->getAnalysisManager()
-																																 ->getNamedConstructAccessFacility()
-																																 ->getConstructsByIdentifyerName(mainMatcher);
-	std::cout << "main Matcher returned " << csByNameMain->size() << " matches" << std::endl;
+void ProgramEntrySelector::finalize() {
+}
+
+void ProgramEntrySelector::execute() {
+	std::list<std::string> mainString = { std::string("main") };
+	auto mainMatcher = InstRO::Tooling::NamedConstructAccess::WildcardedStringMatcher(mainString);
+
+	std::unique_ptr<InstRO::Core::ConstructSet> csByNameMain =
+			getInstrumentorInstance()->getAnalysisManager()->getNamedConstructAccessFacility()->getConstructsByIdentifyerName(
+					mainMatcher);
 
 	// we now have anything that is called main. intersect it with the set of function statements
-	std::unique_ptr<InstRO::Core::ConstructSet> csFunctions =
-			InstRO::getInstrumentorInstance()->getAnalysisManager()->getGrammarInterface()->getConstructsByType(
-					InstRO::Tooling::GrammarInterface::GTFunction);
-	std::cout << "Found " << csFunctions->size() << " functions" << std::endl;
+	auto csFunctions = getInstrumentorInstance()->getAnalysisManager()->getGrammarInterface()->getConstructsByClass(
+			InstRO::Core::ConstructTraitType::CTFunction);
 
-	output = std::make_unique<InstRO::Core::ConstructSet>(csFunctions->intersect(*(csByNameMain.get())));
-	std::cout << "Intersection between both returned " << output->size() << std::endl;
+	output = std::make_unique<InstRO::Core::ConstructSet>(csFunctions->intersect(*csByNameMain));
 }
-Core::ConstructSet* ProgramEntrySelector::getOutput() { return output.get(); }
-void ProgramEntrySelector::releaseOutput() { output->clear(); }
+
+Core::ConstructSet* ProgramEntrySelector::getOutput() {
+	return output.get();
+}
+
+void ProgramEntrySelector::releaseOutput() {
+	output->clear();
+}
 
 }	// Selector
 }	// InstRO
