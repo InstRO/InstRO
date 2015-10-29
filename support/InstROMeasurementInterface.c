ca943286c80b5733a745c5724f550be29a220ccb
Jan Lehr <jp.lehr@gmail.com>
2015-10-29 15:04:36 +0100

    Updated the weak symbol linkage...
    InstRO would now ship a default library which implements all hooks with
    empty bodies. As those are defined as weak symbols they can be easily
    replaced.

---
+++ support/InstROMeasurementInterface.c
@@ -0,0 +1,26 @@
+#include "InstROMeasurementInterface.h"
+
+
+void __instro_start_function(long construct_hash, void *callsite) {}
+
+void __instro_end_function(long construct_hash, void *callsite) {}
+
+void __instro_start_statement(long construct_hash) {}
+
+void __instro_end_statement(long construct_hash) {}
+
+void __instro_start_loop(long construct_hash) {}
+
+void __instro_end_loop(long construct_hash) {}
+
+void __instro_start_scope(long construct_hash) {}
+
+void __instro_end_scope(long construct_hash) {}
+
+void __instro_start_conditional(long construct_hash) {}
+
+void __instro_end_conditional(long construct_hash) {}
+
+void __instro_start_expression(long construct_hash) {}
+
+void __instro_end_expression(long construct_hash) {}
--- support/InstROMeasurementInterface.h
+++ support/InstROMeasurementInterface.h
@@ -83,26 +83,3 @@ void __instro_end_expression(long construct_hash) _INSTRO_WEAK_DECL;
 
 
 
-void __instro_start_function(long construct_hash, void *callsite) {}
-
-void __instro_end_function(long construct_hash, void *callsite) {}
-
-void __instro_start_statement(long construct_hash) {}
-
-void __instro_end_statement(long construct_hash) {}
-
-void __instro_start_loop(long construct_hash) {}
-
-void __instro_end_loop(long construct_hash) {}
-
-void __instro_start_scope(long construct_hash) {}
-
-void __instro_end_scope(long construct_hash) {}
-
-void __instro_start_conditional(long construct_hash) {}
-
-void __instro_end_conditional(long construct_hash) {}
-
-void __instro_start_expression(long construct_hash) {}
-
-void __instro_end_expression(long construct_hash) {}
