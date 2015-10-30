83e4b43fb5a0bf9454f3dbf8713bbee7b1857973
roman <kaossilat0r.rn@gmail.com>
2015-10-30 13:18:01 +0100

    moved ASTHelper to Utility namespace

---
+++ lib/include/instro/rose/utility/ASTHelper.h
@@ -0,0 +1,47 @@
+#ifndef ASTHELPER_H
+#define ASTHELPER_H
+
+#include "rose.h"
+
+#include <string>
+
+namespace InstRO {
+namespace Rose {
+namespace Utility {
+
+/**
+ * \brief methods to simplify handling of AST nodes
+ * \author Roman
+ */
+namespace ASTHelper {
+	/* Return the defining declaration for a given declaration */
+	SgFunctionDeclaration* getDefiningDeclaration(SgFunctionDeclaration* oldDecl);
+	/* Return the first non-defining declaration for a given declaration */
+	SgFunctionDeclaration* getFirstNondefDeclaration(SgFunctionDeclaration* oldDecl);
+
+	/* Return the defining declaration for a given definition */
+	SgFunctionDeclaration* getDefiningDeclaration(SgFunctionDefinition* oldDef);
+	/* Return the first non-defining declaration for a given definition */
+	SgFunctionDeclaration* getFirstNondefDeclaration(SgFunctionDefinition* oldDef);
+
+	/* Return the definition for a given function call expression */
+	SgFunctionDefinition* getFunctionDefinition(SgFunctionCallExp* callExp);
+
+	/* Return the loop nesting level of a statement */
+	int getLoopNestingLevel(SgLocatedNode* n);
+
+	std::string nondefUnparseToString(SgFunctionDeclaration* funcDecl);
+	std::string nondefUnparseToString(SgFunctionDefinition* funcDef);
+
+	std::string getFunctionSignature(SgFunctionDefinition* funcDef);
+
+	/* private helper functions for loop-nesting level */
+	SgScopeStatement* getEnclosingLoop(SgLocatedNode* n);
+	bool isLoop(SgScopeStatement* scope);
+}
+
+}	// namespace Utility
+}	// namespace Rose
+}	// namespace InstRO
+
+#endif	// ASTHELPER_H
