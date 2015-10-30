7377195e7c15c6bdf1b8359121fd092ab37c891c
Jan Lehr <jp.lehr@gmail.com>
2015-10-30 14:27:57 +0100

    Updated PassFactory to comply with minimum InstRO specification

--- lib/include/instro/core/PassFactory.h
+++ lib/include/instro/core/PassFactory.h
@@ -8,19 +8,35 @@
 #include <hash_map>
 
 #include "instro/core/Pass.h"
+#include "instro/core/PassManager.h"
 
 namespace InstRO {
-	class PassManager;
 /* PassFactory: Interface for the mandatory InstRO Passes. */
 class PassFactory {
  public:
 	/* CI: A PassFactory must be initialized with the PassManager. */
 	PassFactory(PassManagement::PassManager* refManager) : passManager(refManager){};
-	virtual Pass* createBlackNWhiteFilter(Pass* input) = 0;
 	virtual Pass* createBlackNWhiteSelector(std::string string) = 0;
-	virtual Pass* createBooleanOrSelector(Pass* inputA, Pass* inputB) = 0;
 	virtual Pass* createProgramEntrySelector() = 0;
 	virtual Pass* createCygProfileAdapter(Pass* input) = 0;
+
+
+	/*
+	 * According to our minimal InstRO compliance file this is the list of components we need to provide.
+	 */
+	virtual Pass* createBooleanAndSelector(Pass *passA, Pass *passB) = 0;
+	virtual Pass* createBooleanOrSelector(Pass *passA, Pass *passB) = 0;
+	virtual Pass* createIdentifierMatcherSelector() = 0;
+	virtual Pass* createCallpathSelector() = 0;
+	virtual Pass* createConstructClassSelector() = 0;
+	virtual Pass* createAggregationStatementCountSelector() = 0;
+
+	virtual Pass* createConstructRaisingElevator() = 0;
+	virtual Pass* createConstructLoweringElevator() = 0;
+	virtual Pass* createConstructCroppingElevator() = 0;
+
+
+	virtual Pass* createInstROMeasurementInterfaceAdapter(Pass *input) = 0;
 
  protected:
 	PassManagement::PassManager* passManager;
