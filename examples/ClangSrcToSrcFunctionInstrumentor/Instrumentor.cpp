5225801a3669227b188e6ee673d2a9c557084a4d
Jan Lehr <jp.lehr@gmail.com>
2015-11-03 15:23:39 +0100

    Made the Clang example simple function instrumentor

--- examples/ClangSrcToSrcFunctionInstrumentor/Instrumentor.cpp
+++ examples/ClangSrcToSrcFunctionInstrumentor/Instrumentor.cpp
@@ -3,6 +3,7 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 
 #include "instro/clang/ClangInstrumentor.h"
+#include "instro/utility/Logger.h"
 
 /*
  * This is meant as something where one can simply look up
@@ -12,22 +13,18 @@
 static llvm::cl::OptionCategory MyTool("my tool");
 
 int main(int argc, char **argv) {
-	std::cout << "Start" << std::endl;
+	InstRO::logIt(InstRO::INFO) << "Start" << std::endl;
 
 	InstRO::Clang::ClangInstrumentor instro(argc, const_cast<const char **>(argv), MyTool);
 
 	auto fac = instro.getFactory();
 	auto fDefSel = fac->createFunctionDefinitionSelector();
-	std::vector<std::string> bList, wList;
-	wList.push_back("foo");
-	auto bwSel = fac->createBlackAndWhiteListSelector(bList, wList);
-	auto bcSel = fac->createBooleanOrSelector(fDefSel, bwSel);
 
-	//	fac->createCygProfileAdapter(fDefSel);
-	fac->createLLVMInputAdapter(fDefSel);
+	fac->createCygProfileAdapter(fDefSel);
+//	fac->createLLVMInputAdapter(fDefSel);
 
 	instro.apply();
 
-	std::cout << "End" << std::endl;
+	InstRO::logIt(InstRO::INFO) << "End" << std::endl;
 	return 0;
 }
