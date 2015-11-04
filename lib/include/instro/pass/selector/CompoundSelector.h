c47e9da7dcd646fef4c74499d57d4cdc91d53355
roman <kaossilat0r.rn@gmail.com>
2015-11-04 13:56:08 +0100

    moved CompoundSelector from Rose namespace

--- lib/include/instro/pass/selector/CompoundSelector.h
+++ lib/include/instro/pass/selector/CompoundSelector.h
@@ -1,5 +1,5 @@
-#ifndef ROSE_COMPOUNDSELECTORS_H
-#define ROSE_COMPOUNDSELECTORS_H
+#ifndef INSTRO_CORE_BOOLEANCOMPOUNDSELECTOR_H
+#define INSTRO_CORE_BOOLEANCOMPOUNDSELECTOR_H
 #include <vector>
 #include <string>
 #include <memory>
@@ -8,22 +8,22 @@
 #include "instro/core/PassImplementation.h"	// ChannelConfiguration
 
 namespace InstRO {
-namespace Rose {
 namespace Selector {
 
-class CompoundSelector : public InstRO::Rose::RosePassImplementation {
+class CompoundSelector : public InstRO::Core::PassImplementation {
 
  public:
 	typedef enum { CO_Or, CO_And, CO_XOr } CompoundOperationType;
+
 	CompoundSelector(Pass* inputA, Pass* inputB, CompoundOperationType Operation)
-			: RosePassImplementation(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
+			: PassImplementation(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
 	CompoundSelector(Pass* inputA, bool invertPassA, Pass* inputB, bool invertPassB, CompoundOperationType Operation)
-			: RosePassImplementation(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
+			: PassImplementation(InstRO::Core::ChannelConfiguration(inputA, inputB)){};
 	void init() override{};
 	void execute() override;
 	void finalize() override{};
 };
-}	// End namespace Selectors
-}	// End namespace Rose
-}	// End namepsace InstRO
-#endif
+}	// namespace Selector
+}	// namepsace InstRO
+
+#endif	// INSTRO_CORE_BOOLEANCOMPOUNDSELECTOR_H
