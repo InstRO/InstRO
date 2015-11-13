496d4658dfd26555091f9cf41230f7da8e129b55
Jan Lehr <jp.lehr@gmail.com>
2015-11-13 10:08:12 +0100

    Moved the supporting classes for RoseTest to own file

--- test/SelectionTestMain.cpp
+++ test/SelectionTestMain.cpp
@@ -1,40 +1,16 @@
+
 #include "instro.h"
-#include "lib/TestAdapter.h"
 #include <iostream>
 
-namespace RoseTest{
 
-/**
- * Some prototyping
- */
-class RoseTestFactory : public InstRO::Rose::RosePassFactory {
-	public:
-		RoseTestFactory(InstRO::PassManagement::PassManager *manager, SgProject *project) : InstRO::Rose::RosePassFactory(manager, project){}
-		InstRO::Pass *createTestAdapter(InstRO::Pass *input, std::set<std::string> expectedItems){
-			auto pImpl = new InstRO::Test::TestAdapter(input, expectedItems);
-			InstRO::Pass *p = new InstRO::Pass(pImpl);
-			p->setPassName("TestAdapter");
-			passManager->registerPass(p);
-			return p;
-		}
-};
-
-class RoseTestInstrumentor : public InstRO::RoseInstrumentor {
-	public:
-		RoseTestInstrumentor(int argc, char **argv) : InstRO::RoseInstrumentor(argc, argv) {}
-
-		RoseTestFactory *getFactory(InstRO::Instrumentor::CompilationPhase phase = InstRO::Instrumentor::CompilationPhase::frontend) override {
-			return new RoseTestFactory(passManager, project);
-		}
-
-};
-}
+#ifdef USE_ROSE
+#include "lib/RoseTestSupport.h"
+#endif
 
 /**
  * This is the TestInstrumentor implementation.
  */
-int main(int argc, char **argv){
-
+int main(int argc, char **argv) {
 /*
  * We want to use the same binary for both Rose and Clang
  */
