e25b568d7005960c29cd733899d6d990e4416b1b
Jan Lehr <jp.lehr@gmail.com>
2015-11-17 10:17:49 +0100

    Reduced noise in instro.h

--- lib/include/instro.h
+++ lib/include/instro.h
@@ -1,54 +1,19 @@
 /** \mainpage
- * InstRO is an instrumentation framework initially based on the ROSE compiler
- *infrastructure.
- * It is in development at TU Darmstadt, Germany.
- * Development started at RWTH Aachen, Germany.
- *
- * define INSTRUMENT_COMPILER_INSTANTIATED_TEMPLATES compiler flag to use InstRO
- *experimental
- * instrumentation of templated c++ code.
- * This depends on ROSE and its capability of handling template sources.
- *
- * Its goal is to provide a powerful tool for performance analysis of C/C++ and
- *Fortran codes.
- *
- *
+*
  */
 #include "instro/config.h"
- 
-
-namespace InstRO {
-namespace Core {};		 // Here we define the core interfaces and classes of instro
-namespace Tooling {};	// Here we define the interface for the classes of the tooling layer. Implementations may reside
-											 // within a tools namespace
-namespace Extra {};		 // This namespace is used for extensions associated with InstRO, such as the visual pass manager
-namespace Utility {};	// Contains util functions, such as reg-expression, file readers, etc.
-namespace Rose {};		 // Contains the implementation for ROSE
-namespace LLVM {};		 // Contains the implementation for LLVM
-namespace CLANG {};		 // Contains the implementation for CLANG
-namespace Example {};	// An example implementation and usage of the InstRO interface without actual functionality. This
-											 // serves to demonstrate what must be implemented, to obtain a minimal functional InstRo
-}
 
 #include "instro/core/Pass.h"
 #include "instro/core/PassFactory.h"
 #include "instro/core/Instrumentor.h"
 
-//#include "instro/core/Pass.h"
-//#include "instro/core/PassFactory.h"
-//#include "instro/core/ConstructSet.h"
-//#include "instro/core/Factory.h"
-//#include "instro/core/Instrumentor.h"
-
-//#include "instro/core/PassManager.h"
-
 #ifdef USE_ROSE
-	#if !defined(USING_EXAMPLE)
-		#include "instro/rose/core/RosePassImplementation.h"
-		#include "instro/rose/RosePassFactory.h"
-		#include "instro/rose/RoseInstrumentor.h"
-		#include "instro/rose/pass/adapter/RoseConstructPrinter.h"
-	#endif
+#if !defined(USING_EXAMPLE)
+#include "instro/rose/core/RosePassImplementation.h"
+#include "instro/rose/RosePassFactory.h"
+#include "instro/rose/RoseInstrumentor.h"
+#include "instro/rose/pass/adapter/RoseConstructPrinter.h"
+#endif
 #endif
 
 #ifdef USING_LLVM
@@ -56,7 +21,6 @@ namespace Example {};	// An example implementation and usage of the InstRO inter
 #endif
 
 #ifdef USING_EXAMPLE
-//#include "instro/example/ExampleConstructProvider.h"
 #include "instro/example/ExampleInstrumentor.h"
 #include "instro/example/ExamplePass.h"
 #include "instro/example/ExamplePassFactory.h"
