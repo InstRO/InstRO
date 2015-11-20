06d4106de8688f3d8eaf7bfcfd159e3704e0d88d
Jan Lehr <jp.lehr@gmail.com>
2015-11-20 16:22:57 +0100

    Removed the Example implementation..
    .. removed the non-functional maitenance burden.

--- lib/src/example/ExampleInstrumentor.cpp
+++
@@ -1,7 +0,0 @@
-#include "instro/example/ExampleInstrumentor.h"
-namespace InstRO {
-void ExampleInstrumentor::init() {}
-void ExampleInstrumentor::finalize() {}
-
-void ExampleInstrumentor::apply() { passManager->execute(); }
-}
