c37ec2a151faa03d9ced6fa27252bfb380593c02
Jan Lehr <jp.lehr@gmail.com>
2015-11-27 18:06:15 +0100

    Changed CLExpressionPredicate for readability..
    ... IDK why there were redundant checks. Removed those. We should talk
    about the predicate in general.

--- lib/include/instro/rose/core/RoseConstructSet.h
+++ lib/include/instro/rose/core/RoseConstructSet.h
@@ -50,7 +50,7 @@ struct CLExpressionPredicate : public CTPredicate {
 			return true;
 		// for variables and values, we only accept as instrumentable, if the expression itself has an observable effect,
 		// e.g. as conditional in an if or for
-		if (isSgValueExp(n) != nullptr || isSgIntVal(n) != nullptr || isSgStringVal(n) != nullptr || isSgVarRefExp(n) != nullptr) {
+		if (isSgValueExp(n) != nullptr || isSgVarRefExp(n) != nullptr) {
 			// In Rose this is TRUE !!if!! the parent of the stmt is an SgExprStatement and the parent(parent) is either the for loops
 			// conditional or the conditional of an if or while
 			SgNode* parent = n->get_parent();
