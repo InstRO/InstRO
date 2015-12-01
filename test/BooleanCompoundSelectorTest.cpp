c886587fe9723031eb99efd661f66e80f0f22975
Jan Lehr <jp.lehr@gmail.com>
2015-12-01 18:04:19 +0100

    Added test for BooleanCompoundSelector and cleaned up Makefile.am

---
+++ test/BooleanCompoundSelectorTest.cpp
@@ -0,0 +1,43 @@
+
+#include "instro.h"
+#include <iostream>
+
+#ifdef USE_ROSE
+#include "lib/RoseTestSupport.h"
+#endif
+
+#include "instro/utility/Logger.h"
+#include "instro/utility/Environment.h"
+
+int main(int argc, char **argv) {
+/*
+ * We want to use the same binary for both Rose and Clang
+ */
+#ifdef USE_ROSE
+	using InstrumentorType = RoseTest::RoseTestInstrumentor;
+	using CTrait = InstRO::Core::ConstructTraitType;
+
+	InstrumentorType instrumentor(argc, argv);
+	auto factory = instrumentor.getFactory();
+
+	std::string filename = InstRO::Utility::getEnvironmentVariable("INSTRO_TEST_INPUT_FILENAME");
+
+	auto selA = factory->createConstructClassSelector(CTrait::CTFunction);
+	auto selB = factory->createConstructClassSelector(CTrait::CTStatement);
+
+	auto andSelector = factory->createBooleanAndSelector(selA, selB);
+	auto orSelector = factory->createBooleanOrSelector(selA, selB);
+
+	factory->createTestAdapter(andSelector, "FunctionAndStatementSelector", filename);
+	factory->createTestAdapter(orSelector, "FunctionOrStatementSelector", filename);
+
+	instrumentor.apply();
+
+	return instrumentor.testFailed();
+#endif
+
+#ifdef USE_CLANG
+	logIt(ERROR) << "Not implemented yet!" << std::endl;
+	return 0;
+#endif
+}
