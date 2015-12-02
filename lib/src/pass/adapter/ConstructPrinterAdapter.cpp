2ef86d160a1f5ff9c9e6312a885a9033cc4e706f
roman <kaossilat0r.rn@gmail.com>
2015-12-02 16:47:32 +0100

    even better formatting for constructPrinterAdapter

--- lib/src/pass/adapter/ConstructPrinterAdapter.cpp
+++ lib/src/pass/adapter/ConstructPrinterAdapter.cpp
@@ -3,6 +3,7 @@
 #include "instro/core/ConstructSet.h"
 
 #include <iostream>
+#include <iomanip>	// setw
 
 namespace InstRO {
 namespace Adapter {
@@ -13,7 +14,7 @@ void ConstructPrinterAdapter::execute() {
 	auto minLevel = inputPass->getOutput()->getMinConstructLevel();
 	auto maxLevel = inputPass->getOutput()->getMaxConstructLevel();
 
-	std::cout << "[ConstructPrinter] ConstructSet contains " << cs.size() << " constructs";
+	std::cout << "=ConstructPrinter= ConstructSet contains " << cs.size() << " constructs";
 	if (!cs.empty()) {
 		std::cout << " | min: " << InstRO::Core::constructLevelToString(minLevel)
 							<< " | max: " << InstRO::Core::constructLevelToString(maxLevel) << std::endl;
@@ -23,8 +24,8 @@ void ConstructPrinterAdapter::execute() {
 
 	unsigned long count = 0;
 	for (auto construct : cs) {
-		std::cout << count << ": " << construct->getTraits().toStringShort()
-							<< ":\t" << construct->toString() << std::endl;
+		std::cout << std::setw(4) << count << ": " << construct->getTraits().toStringShort()
+							<< ":  " << construct->toString() << std::endl;
 		count++;
 	}
 }
