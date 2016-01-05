4c6e046c8f7fd2bc9b7ea24381d2782851b3558e
Jan Lehr <jp.lehr@gmail.com>
2016-01-05 10:30:48 +0100

    Only reorganizing class members.

--- lib/include/instro/core/PassImplementation.h
+++ lib/include/instro/core/PassImplementation.h
@@ -1,9 +1,9 @@
 #ifndef INSTRO_CORE_PASSIMPLEMENTATION_H
 #define INSTRO_CORE_PASSIMPLEMENTATION_H
+
 #include <initializer_list>
 #include <unordered_map>
-// Pass.h is included at the End of this file. We use forward declarations to break the circle Pass - PassImplementation
-// - Pass
+
 #include "instro/core/ConstructSet.h"
 #include "instro/utility/MemoryManagement.h"
 
@@ -14,29 +14,25 @@ class Pass;
 namespace Core {
 
 /*
- * The class serves as interface between Pass and PassImplementation to make them independent of the actual input channel
+ * The class serves as interface between Pass and PassImplementation to make them independent of the actual input
+ * channel
  */
 class ChannelConfiguration {
- protected:
-	std::vector<Pass *> inputChannelPasses;
-	std::unordered_map<Pass *, InstRO::Core::ConstructTraitType> inputChannelMin;
-	std::unordered_map<Pass *, InstRO::Core::ConstructTraitType> inputChannelMax;
-
  public:
 	// CI: Empty Configuration - No Input Passes used
 	ChannelConfiguration(){};
 
 	ChannelConfiguration(Pass *p1) {
 		inputChannelPasses.push_back(p1);
-		inputChannelMin[p1] = ::InstRO::Core::ConstructTraitType::CTMin;
-		inputChannelMax[p1] = ::InstRO::Core::ConstructTraitType::CTMax;
+		inputChannelMin[p1] = InstRO::Core::ConstructTraitType::CTMin;
+		inputChannelMax[p1] = InstRO::Core::ConstructTraitType::CTMax;
 	}
 
 	template <class... PassList>
 	ChannelConfiguration(Pass *p1, PassList... passes) {
 		inputChannelPasses.insert(inputChannelPasses.begin(), {p1, passes...});
 
-		std::vector<Pass*> allPasses = {p1, passes...};
+		std::vector<Pass *> allPasses = {p1, passes...};
 		for (auto pass : allPasses) {
 			inputChannelMin[pass] = InstRO::Core::ConstructTraitType::CTMin;
 			inputChannelMax[pass] = InstRO::Core::ConstructTraitType::CTMax;
@@ -44,18 +40,19 @@ class ChannelConfiguration {
 	}
 
 	template <class Iterator>
-	ChannelConfiguration(Iterator begin, Iterator end, InstRO::Core::ConstructTraitType minLevel, InstRO::Core::ConstructTraitType maxLevel) {
-        for (Iterator iter = begin; iter != end; ++iter) {
-            inputChannelPasses.push_back(*iter);
-            inputChannelMin[*iter] = minLevel;
-            inputChannelMax[*iter] = maxLevel;
-        }
+	ChannelConfiguration(Iterator begin, Iterator end, InstRO::Core::ConstructTraitType minLevel,
+											 InstRO::Core::ConstructTraitType maxLevel) {
+		for (Iterator iter = begin; iter != end; ++iter) {
+			inputChannelPasses.push_back(*iter);
+			inputChannelMin[*iter] = minLevel;
+			inputChannelMax[*iter] = maxLevel;
+		}
 	}
 
 	struct PassMinMaxSequenceHelper {
-		::InstRO::Pass *pass;
-		::InstRO::Core::ConstructTraitType min;
-		::InstRO::Core::ConstructTraitType max;
+		InstRO::Pass *pass;
+		InstRO::Core::ConstructTraitType min;
+		InstRO::Core::ConstructTraitType max;
 	};
 
 	template <class... PassList>
@@ -67,14 +64,19 @@ class ChannelConfiguration {
 		}
 	}
 
-	void setConstructLevel(Pass *inputPass, ::InstRO::Core::ConstructTraitType minLevel,
-												 ::InstRO::Core::ConstructTraitType maxLevel) {
+	void setConstructLevel(Pass *inputPass, InstRO::Core::ConstructTraitType minLevel,
+												 InstRO::Core::ConstructTraitType maxLevel) {
 		inputChannelMin[inputPass] = minLevel;
 		inputChannelMax[inputPass] = maxLevel;
 	}
-	::InstRO::Core::ConstructTraitType getMinConstructLevel(Pass *inputPass) { return inputChannelMin[inputPass]; }
-	::InstRO::Core::ConstructTraitType getMaxConstructLevel(Pass *inputPass) { return inputChannelMax[inputPass]; }
+	InstRO::Core::ConstructTraitType getMinConstructLevel(Pass *inputPass) { return inputChannelMin[inputPass]; }
+	InstRO::Core::ConstructTraitType getMaxConstructLevel(Pass *inputPass) { return inputChannelMax[inputPass]; }
 	std::vector<::InstRO::Pass *> const getPasses() { return inputChannelPasses; };
+
+ protected:
+	std::vector<Pass *> inputChannelPasses;
+	std::unordered_map<Pass *, InstRO::Core::ConstructTraitType> inputChannelMin;
+	std::unordered_map<Pass *, InstRO::Core::ConstructTraitType> inputChannelMax;
 };
 
 /*
@@ -84,9 +86,8 @@ class ChannelConfiguration {
  * predecessors.
  */
 class PassImplementation {
-public:
-	PassImplementation(ChannelConfiguration channelConfig)
-		: channelConfig(channelConfig), collisionSet() {}
+ public:
+	PassImplementation(ChannelConfiguration channelConfig) : channelConfig(channelConfig), collisionSet() {}
 	PassImplementation() = delete;
 	virtual ~PassImplementation() {}
 
@@ -97,25 +98,23 @@ public:
 	virtual void releaseOutput() { outputSet.clear(); }
 	ChannelConfiguration getChannelConfig() { return channelConfig; }
 
-	virtual ConstructSet* getOutput() { return &outputSet; };
-	ConstructSet* getInput(Pass *pId);
+	virtual ConstructSet *getOutput() { return &outputSet; };
+	ConstructSet *getInput(Pass *pId);
 
-private:
-	ChannelConfiguration channelConfig;
+	const ConstructSet *cgetCollisionSet() { return &collisionSet; };
 
-protected:
+ protected:
 	ConstructSet outputSet;
-	
+
 	ChannelConfiguration &getChannelCFG() { return channelConfig; }
 
 	// This set is used to track alterations to the AST and notify which nodes have been invalidated
 	ConstructSet collisionSet;
-	ConstructSet* getCollisionSet() { return &collisionSet; }
-public:
-	const ConstructSet* cgetCollisionSet() { return &collisionSet; };
+	ConstructSet *getCollisionSet() { return &collisionSet; }
 
+ private:
+	ChannelConfiguration channelConfig;
 };
-
 
 }	// namespace Core
 }	// namespace InstRO
