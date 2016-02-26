07a9be015892cd385d4e9f8a4c06f2fa40e83e7a
Jan Lehr <jp.lehr@gmail.com>
2016-02-26 13:30:50 +0100

    Added calls to cyg profile in our deflt lib implementation

--- support/InstROMeasurementInterface.c
+++ support/InstROMeasurementInterface.c
@@ -12,22 +12,42 @@ void __instro_end_function(size_t construct_hash, void *callsite) {
 	__cyg_profile_func_exit((void *) construct_hash, callsite);
 }
 
-void __instro_start_simple_statement(size_t construct_hash) {}
+void __instro_start_simple_statement(size_t construct_hash) {
+	__cyg_profile_func_enter((void *) construct_hash, 0);
+}
 
-void __instro_end_simple_statement(size_t construct_hash) {}
+void __instro_end_simple_statement(size_t construct_hash) {
+	__cyg_profile_func_exit((void *) construct_hash, 0);
+}
 
-void __instro_start_loop(size_t construct_hash) {}
+void __instro_start_loop(size_t construct_hash) {
+	__cyg_profile_func_enter((void *) construct_hash, 0);
+}
 
-void __instro_end_loop(size_t construct_hash) {}
+void __instro_end_loop(size_t construct_hash) {
+	__cyg_profile_func_exit((void *) construct_hash, 0);
+}
 
-void __instro_start_scope(size_t construct_hash) {}
+void __instro_start_scope(size_t construct_hash) {
+	__cyg_profile_func_enter((void *) construct_hash, 0);
+}
 
-void __instro_end_scope(size_t construct_hash) {}
+void __instro_end_scope(size_t construct_hash) {
+	__cyg_profile_func_exit((void *) construct_hash, 0);
+}
 
-void __instro_start_conditional(size_t construct_hash) {}
+void __instro_start_conditional(size_t construct_hash) {
+	__cyg_profile_func_enter((void *) construct_hash, 0);
+}
 
-void __instro_end_conditional(size_t construct_hash) {}
+void __instro_end_conditional(size_t construct_hash) {
+	__cyg_profile_func_exit((void *) construct_hash, 0);
+}
 
-void __instro_start_expression(size_t construct_hash) {}
+void __instro_start_expression(size_t construct_hash) {
+	__cyg_profile_func_enter((void *) construct_hash, 0);
+}
 
-void __instro_end_expression(size_t construct_hash) {}
+void __instro_end_expression(size_t construct_hash) {
+	__cyg_profile_func_exit((void *) construct_hash, 0);
+}
