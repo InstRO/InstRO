a58718f09c1b5718223b36386512ce7f6e1255fd
Simon <simon.reuss90@gmx.de>
2016-03-21 16:21:25 +0100

    Fixed Logger
    The Logger class previously relied on undefined behavior as it did not have an explicit constructor and did not call the super constructor (std::ostream)

--- lib/include/instro/utility/Logger.h
+++ lib/include/instro/utility/Logger.h
@@ -5,39 +5,30 @@
 
 namespace InstRO {
 
-enum LogLevel {
-	ERROR,
-	WARN,
-	INFO,
-	DEBUG
-};
+enum LogLevel { ERROR, WARN, INFO, DEBUG };
 
 /* Default Log Level */
 #ifndef INSTRO_LOG_LEVEL
-#define INSTRO_LOG_LEVEL   WARN
+#define INSTRO_LOG_LEVEL WARN
 #endif
 
-/*
+/**
  * \brief The visible logLevel is configurable per translation unit.
  * \author Roman Ness
  */
-class Logger: public std::ostream {
-public:
-	void setLevel(enum LogLevel level) {
-		this->level = level;
-	}
+class Logger : public std::ostream {
+ public:
+	Logger() : std::ostream(std::cout.rdbuf()) {}
+	void setLevel(enum LogLevel level) { this->level = level; }
 
-	void setThreshold(enum LogLevel threshold) {
-		this->threshold = threshold;
-	}
+	void setThreshold(enum LogLevel threshold) { this->threshold = threshold; }
 
-private:
+ private:
 	enum LogLevel level;
 	enum LogLevel threshold;
 
-public:
-
-	template<typename T>
+ public:
+	template <typename T>
 	Logger& operator<<(T const& val) {
 		if (level <= threshold) {
 			std::cout << val;
@@ -45,19 +36,18 @@ public:
 		return *this;
 	}
 
-	Logger& operator<<(std::ostream& (val)(std::ostream&)) {
+	Logger& operator<<(std::ostream&(val)(std::ostream&)) {
 		if (level <= threshold) {
 			std::cout << val;
 		}
 		return *this;
 	}
-
 };
 
 Logger& logIt(enum LogLevel level, enum LogLevel localThreshold = INSTRO_LOG_LEVEL);
 
-} // namespace InstRO
+}	// namespace InstRO
 
 #undef INSTRO_LOG_LEVEL
 
-#endif // INSTRO_UTILITY_LOG_H_
+#endif	// INSTRO_UTILITY_LOG_H_
